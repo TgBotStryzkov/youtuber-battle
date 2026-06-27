92aa826db42446dac6ee04f92a592c8e1cc98ab7
TgBotStryzkov <mihailstryzkov@gmail.com>
2026-06-27 20:13:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const SUPABASE_URL = "https://qvmclzjvnmkmtxhrazwr.supabase.co";
+const SUPABASE_KEY = "sb_publishable_6PDzwQ-x9cL3e8ft5k5kFg_0UjCQNFT";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const bloggers = [
   {
     name: "MrBeast",
